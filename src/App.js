cd98120b4875b336d40226b8ed75d18ff970d40a
spooky <zxoocy@gmail.com>
2024-07-23 18:13:14 +0700

    select + post sorting

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
     const [posts, setPosts] = React.useState([
@@ -11,37 +13,44 @@ function App() {
         {id: 3, title: 'Post Item 3', body: 'Lorem ipsum dolor sit amet.'},
     ]);
 
-    const [title, setTitle] = React.useState('');
-    const [body, setBody] = React.useState('');
+    const [selectedSort, setSelectedSort] = React.useState('')
 
-    const AddNewPost = (e) => {
-        e.preventDefault();
-        const newPost = {
-            id: Date.now(),
-            title,
-            body,
-        }
+    const createPost = (newPost) => {
         setPosts([...posts, newPost]);
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
+    }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
     }
 
   return (
     <div className="App">
-        <form>
-            <MyInput
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                type="text"
-                placeholder="Название поста"
+        <PostForm create={createPost}/>
+        <hr style={{margin: '15px 0'}}/>
+        <div>
+            <MySelect
+                value={selectedSort}
+                onChange={sortPosts}
+                defaultValue="Сортировка"
+                options={[
+                    {value: 'title', name: 'По названию'},
+                    {value: 'body', name: 'По описанию'},
+                ]}
             />
-            <MyInput
-                value={body}
-                onChange={(e) => setBody(e.target.value)}
-                type="text"
-                placeholder="Описание поста"
-            />
-            <MyButton onClick={AddNewPost} >Создать пост</MyButton>
-        </form>
-        <PostList posts={posts} title="Список постов 1" />
+        </div>
+        {posts.length !== 0
+        ?
+            <PostList remove={removePost} posts={posts} title="Список постов 1" />
+        :
+            <h1 style={{textAlign: 'center'}}>
+                Посты не найдены!
+            </h1>
+        }
     </div>
   );
 }
---
+++ src/components/PostForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import MyInput from "./UI/input/MyInput";
+import MyButton from "./UI/button/MyButton";
+
+const PostForm = ({create}) => {
+    const [post, setPost] = React.useState({title: '', body: ''});
+
+    const AddNewPost = (e) => {
+        e.preventDefault();
+
+        const newPost = {
+            ...post, id: Date.now(),
+        }
+        create(newPost);
+        setPost({title: '', body: ''});
+    }
+
+    return (
+        <form>
+            <MyInput
+                value={post.title}
+                onChange={(e) => setPost({...post, title: e.target.value})}
+                type="text"
+                placeholder="Название поста"
+            />
+            <MyInput
+                value={post.body}
+                onChange={(e) => setPost({...post, body: e.target.value})}
+                type="text"
+                placeholder="Описание поста"
+            />
+            <MyButton onClick={AddNewPost}>Создать пост</MyButton>
+        </form>
+    );
+};
+
+export default PostForm;
